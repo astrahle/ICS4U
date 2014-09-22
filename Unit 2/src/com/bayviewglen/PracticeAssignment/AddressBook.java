be8e3e57e177635fc0c1434d6a17368a941f6dbc
astrahle <astrahle@bayviewglen.ca>
2014-09-22 15:17:31 -0400

    Initial PracticeAssignment Commit

---
+++ Unit 2/src/com/bayviewglen/PracticeAssignment/AddressBook.java
@@ -0,0 +1,10 @@
+package com.bayviewglen.PracticeAssignment;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ Unit 2/src/com/bayviewglen/PracticeAssignment/Contacts.java
@@ -0,0 +1,15 @@
+package com.bayviewglen.PracticeAssignment;
+
+public class Contacts {
+	private String lname;
+	private String fname;
+	private String phone;
+	
+		public Contacts(){
+			lname = null;
+			fname = null;
+			phone = null;
+		}
+	
+
+}
