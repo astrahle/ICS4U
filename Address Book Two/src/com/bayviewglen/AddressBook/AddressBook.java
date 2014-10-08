1c355bd0ebe948fc488277e7b947956ef1900ed8
astrahle <astrahle@bayviewglen.ca>
2014-10-08 17:38:37 -0400

    Started work on AddressBookDriver

--- Address Book Two/src/com/bayviewglen/AddressBook/AddressBook.java
+++ Address Book Two/src/com/bayviewglen/AddressBook/AddressBook.java
@@ -3,60 +3,84 @@ package com.bayviewglen.AddressBook;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 
-	public class AddressBook {
+public class AddressBook {
 
-		static Map<String, Contact> listMap = new HashMap<String, Contact>();
+	Map<String, Contact> listMap = new HashMap<String, Contact>();
 
-		// method to add a contact to the list (done)
-		public void addContact(String firstName, String lastName, String phone){
-			listMap.put(firstName, new Contact(firstName, lastName, phone));
-			
-		}
-
-		// done
-		public Contact searchByName(String name){
-			if (listMap.containsKey(name)){
-				return listMap.get(name);
-			}
-			else 
-				return null;
-		}
-				
-
-		public void searchByNumber(String phoneNum){
-			for (Contact c:list){
-				if(c.getFirstName().equals(phoneNum)){
-					c.showContact();
-				}
-			}
-			System.out.println("Sorry no one wih the phone number: " + phoneNum + "exists.");
-		}
-
-		public static void removeContact (Contact toRemove){
-			for (int x = 0; x < list.size(); x++){
-				if (list.get(x).equals(toRemove)){
-					list.remove(x);
-					break;
-				}
-			}
-			System.out.println("Sorry we could not delete that contact since they don't exist.");
-		}
-
-		public void displayAllContacts(){
-			for (int x = 0; x < list.size(); x++){
-				System.out.println((x+1)+ ",");
-				list.get(x).showContact();
-				System.out.println("--------------------------------");
-			}
-		}
-
+	// method to add a contact to the list (done)
+	public void addContact(String lastName, String firstName, String phone){
+		listMap.put(phone, new Contact(lastName, firstName, phone));
 
 	}
+
+	// done
+	public Contact searchByPhoneNumber(String phone){
+		if (listMap.containsKey(phone)){
+			System.out.println(listMap.get(phone));
+			return listMap.get(phone);
+		}
+		else 
+			return null;
+	}
+
+	// Returns the key of the places where it found a similar object
+	// Need to find way to return the key of the person searched.
+	public Contact searchByFirstName(String firstName){
+		if (listMap.containsValue(firstName)){  
+			List <String> keys = new ArrayList<String>();
+			for (String key : listMap.keySet()){
+				if (listMap.get(key).equals(firstName)){
+					keys.add(key);
+				}
+			}
+			System.out.println("People with the first name " + firstName + " are...");
+			System.out.println(keys);
+			return listMap.get(firstName); //this is where the fix needs to be
+		}
+		else{
+			return null;
+		}
+
+	}
+
+	// Returns the key of the place where it found a similar object
+	// Need to find way to return the key of the person searched.
+	public Contact searchByLastName(String lastName){
+		if (listMap.containsValue(lastName)){  
+			return listMap.get(lastName); //this is where the fix needs to be
+		}
+		else{
+			return null;
+		}
+
+	}
+
+	// Removes the contact at the given key
+	public void removeContact (Contact toRemove){
+		if (listMap.containsKey(toRemove)){
+			listMap.remove(toRemove);
+		}
+		else{
+			System.out.println("Sorry we could not delete " + toRemove + " because they dont exist.");
+		}
+
+	}
+	// i think this works... famous last words
+	public void displayAllContacts(){
+		for (String key : listMap.keySet()){
+			System.out.println(key + listMap.get(key));
+		}
+	}
+
+
+}
 
 
 
--- Address Book Two/src/com/bayviewglen/AddressBook/AddressBookDriver.java
+++ Address Book Two/src/com/bayviewglen/AddressBook/AddressBookDriver.java
@@ -1,5 +1,72 @@
 package com.bayviewglen.AddressBook;
 
+import java.util.Scanner;
+
 public class AddressBookDriver {
 
+	public static void main(String[] args){
+		
+		AddressBook AddressBook = new AddressBook();
+		
+		
+		boolean done = false;
+		
+		System.out.println("Welcome to the address book program.");
+		
+		while (!done){
+			
+			String mainChoice;
+			
+			Scanner choiceScanner = new Scanner(System.in);
+			Scanner firstNameScanner = new Scanner(System.in);
+			Scanner lastNameScanner = new Scanner(System.in);
+			Scanner phoneNumberScanner = new Scanner(System.in);
+			
+			System.out.println(" ");
+			System.out.println("------------------------------------");
+			System.out.println("What would you like to do?");
+			System.out.println("1) Search By Phone Number");
+			System.out.println("2) Search By First Name");
+			System.out.println("3) Search By Last Name");
+			System.out.println("4) Add Contact");
+			System.out.println("5) Remove Contact");
+			System.out.println("6) Display All Contacts");
+			System.out.println("7) Quit");
+			mainChoice = choiceScanner.nextLine();
+			
+			if (mainChoice .equals("1")){
+				String searchedPhone;
+				System.out.print("Please enter the Phone Number of the person you want to search for:");
+				searchedPhone = choiceScanner.nextLine();
+				
+				if (AddressBook.searchByPhoneNumber(searchedPhone) == null){
+					System.out.println("Sorry nobody with that phone number exists...");
+				}
+				if (AddressBook.searchByPhoneNumber(searchedPhone) != null){
+					//System.out.println(AddressBook.searchByPhoneNumber(searchedPhone));
+				}
+			}
+			
+			if (mainChoice . equals("2")){
+				
+			}
+			
+			if (mainChoice .equals("4")){
+				String firstName;
+				String lastName;
+				String phoneNumber;
+				
+				System.out.print("Please enter their first name:");
+				firstName = firstNameScanner.nextLine();
+				
+				System.out.print("Please enter their last name:");
+				lastName = lastNameScanner.nextLine();
+				
+				System.out.print("Please enter their phone number:");
+				phoneNumber = phoneNumberScanner.nextLine();
+				
+				AddressBook.addContact(lastName, firstName, phoneNumber);
+			}
+		}
+	}
 }
